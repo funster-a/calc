af7af345db04d8222ab3d91864c3ca4cf33ea736
funster-a <82862611+funster-a@users.noreply.github.com>
2024-03-09 11:35:34 +0500

    create a basic html css calculator

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 function add(num1, num2) {
+    let firstNumber, secondNumber, operator;
     return num1 + num2
 }
 function subtract(num1, num2) {
+    let firstNumber, secondNumber, operator;
     return num1 - num2
     
 }
 function multiply(num1, num2) {
+    let firstNumber, secondNumber, operator;
     return Math.round(num1 * num2 * 100) / 100
     
     
 }
 function divide(num1, num2) {
+    let firstNumber, secondNumber, operator;
     return Math.round(num1 / num2 * 100) / 100
     
+}
+
+function operate() {
+    let firstNumber = "";
+    let secondNumber = "";
+    let operator = "";
+    add(1, 2)
 }
 
 console.log(add(2, 3))
